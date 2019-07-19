e3cb422070e33e7ddb6f19546ddb126e2cf06fcb
Richard Pos <richardnottoknow@gmail.com>
2019-07-19 11:14:36 +0200

    Finished section 18 part 224

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-
-import App from "./components/App";
-import reducers from "./reducers";
-
-const store = createStore(reducers);
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';   
+import { composeWithDevTools } from 'redux-devtools-extension';    //for redux devtool
+ 
+import App from './components/App';
+import reducers from './reducers';
+ 
+//here we are using composeWithDevTools from npm package to use the devtool
+const store = createStore(reducers, composeWithDevTools(applyMiddleware()));
+ 
+const root = document.querySelector('#root');
+ 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>, root);
