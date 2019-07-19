c45ad0f9d168c9e1234adc4909f9e1b6854f33fd
Richard Pos <richardnottoknow@gmail.com>
2019-07-19 14:08:02 +0200

    Finished section 19 untill part 236

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  );
-};
+class StreamCreate extends React.Component {
+  // below is changed see part 230
+  // renderInput(formProps) {
+  //   return (
+  //     <input 
+  //       onChange={ formProps.input.onChange }
+  //       value={ formProps.input.value }
+  //     />
+  //   )
+  // }
 
-export default StreamCreate;
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    
+    return (
+      <div className={ className }>
+        <label>{ label }</label>
+        <input { ...input } autoComplete="off" />
+        { this.renderError(meta) }
+      </div>
+    )
+  }
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{ error }</div>
+        </div>
+      )
+    }
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues)
+  }
+
+  render() {
+    return (
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error">
+          <Field
+            name="title"
+            component={ this.renderInput }
+            label="Enter title: "
+          />
+          <Field
+            name="description"
+            component={ this.renderInput }
+            label="Enter description: "
+          />
+          <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {}
+
+  if (!formValues.title) {
+    errors.title = "Title can not be blank!!";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Description can not be blank!!";
+  }
+  return errors;
+}
+
+export default reduxForm({
+  form: "streamCreate",
+  validate: validate
+})(StreamCreate);
