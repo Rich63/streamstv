5f02a0a9e65b60cfd01cfa7831fc2db8b72e021e
Richard Pos <richardnottoknow@gmail.com>
2019-07-22 10:18:38 +0200

    Tussentijds voor part 251 adding userId to streams

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
-  // below is changed see part 230
-  // renderInput(formProps) {
-  //   return (
-  //     <input 
-  //       onChange={ formProps.input.onChange }
-  //       value={ formProps.input.value }
-  //     />
-  //   )
-  // }
 
   renderInput = ({ input, label, meta }) => {
     const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    
+
     return (
       <div className={ className }>
         <label>{ label }</label>
@@ -34,9 +27,9 @@ class StreamCreate extends React.Component {
     }
   }
 
-  onSubmit(formValues) {
-    console.log(formValues)
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -72,7 +65,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
