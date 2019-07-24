6bc7c9ab846ae9350cea9ba9034e63a008ec2bc2
Richard Pos <richardnottoknow@gmail.com>
2019-07-24 11:02:16 +0200

    changing StreamDelete to a class based component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import history from "../../history";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    // <React.Fragment> can be replaced by <>
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+    // </React.Fragment> can be replaced by </>
+  );
+
   return (
-    <div>StreamDelete</div>
+    <div>
+      StreamDelete
+      <Modal 
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={ actions }
+        onDismiss={ () => history.push("/") }
+      />
+    </div>
   );
 };
 
